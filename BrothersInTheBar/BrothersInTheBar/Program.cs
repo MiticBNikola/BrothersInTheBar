9cf92c8ef5ceea658ff86fc918c63c18561c21d6
MiticNikola16228 <mitic.nikola@elfak.rs>
2020-03-20 19:24:21 +0100

    Problem solved

--- BrothersInTheBar/BrothersInTheBar/Program.cs
+++ BrothersInTheBar/BrothersInTheBar/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BrothersInTheBar
 {
     class Program
     {
+        public int brothersInTheBar(int[] glasses)
+        {
+            int result = 0;
+            int[] front;
+            int[] back;
+            int n = glasses.Length;
+            int i = 0;
+            while (n >= 3 && i < n - 2)
+            {
+                if (glasses[i] == glasses[i + 1] && glasses[i + 1] == glasses[i + 2])
+                {
+                    result++;
+                    front = glasses.Take(i).ToArray();
+                    back = glasses.Skip(i + 3).Take(n - i - 3).ToArray();
+                    front.CopyTo(glasses, 0);
+                    back.CopyTo(glasses, front.Length);
+                    n = glasses.Length;
+                    if (i > 1)
+                    {
+                        if (glasses[i - 2] == glasses[i - 1])
+                            i = i - 2;
+                        else
+                            i = i - 1;
+                    }
+                    else
+                        i = 0;
+                }
+                else if (glasses[i + 1] == glasses[i + 2])
+                {
+                    i++;
+                }
+                else
+                {
+                    i = i + 2;
+                }
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
+            Program myProgram = new Program();
+            int[] glasses = new int[] { 1, 1, 2, 3, 3, 3, 2, 2, 1, 1 };
+            int result = 0;
+            result = myProgram.brothersInTheBar(glasses);
+            Console.WriteLine("Number of rounds is " + result);
+            Console.ReadLine();
         }
     }
 }
